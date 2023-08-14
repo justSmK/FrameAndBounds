53e1757cca144e43ef0f23e9acdf234b57812088
justSmK <28645140+justSmK@users.noreply.github.com>
2023-08-14 19:30:11 +0300

    Create project
    Project initialized, deleted storyboard, setup scene delegate

---
+++ FrameVsBounds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FrameVsBounds
+//
+//  Created by Sergei Semko on 8/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
